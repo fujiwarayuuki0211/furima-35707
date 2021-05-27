c72a901a3a2ea49dfb064798387f315a29a3b7ac
fujiwarayuuki0211 <respectingevery8@gmail.com>
2021-05-27 14:31:01 +0900

    deviseのユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  private
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birth_date])
+  end
 end
