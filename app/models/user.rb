344d6e6b803d69548ec2b42fa70f19a915d5bc38
fujiwarayuuki0211 <respectingevery8@gmail.com>
2021-05-28 15:03:07 +0900

    コードレビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birth_date
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :kana_first_name 
+    validates :kana_last_name
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_091957) do
+ActiveRecord::Schema.define(version: 2021_05_27_124503) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
