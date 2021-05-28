8dab68f2bd9da31fd5774a7aa3ec63ee2ef21ceb
fujiwarayuuki0211 <respectingevery8@gmail.com>
2021-05-28 16:06:05 +0900

    itemsテーブルの作成

--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 | status_id   | integer    | null: false       |
 | burden_id   | integer    | null: false       |
 | area_id     | integer    | null: false       |
-| days_id     | integer    | null: false       |
+| day_id      | integer    | null: false       |
 | price       | integer    | null: false       |
 | user        | references | foreign_key: true |
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2021_05_27_124503) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "burden_id", null: false
+    t.integer "area_id", null: false
+    t.integer "day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_05_27_124503) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
